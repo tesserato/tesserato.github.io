da8a6f0c0d3be16b46cfdbbd554c5599301c3931
tesserato <tesserato@hotmail.com>
2018-06-28 04:07:04 -0300

    conversão para html corrigida

--- Convert_to_docx.cmd
+++ Convert_to_docx.cmd
@@ -1,3 +1,5 @@
-REM pandoc -o Dissert.docx Dissert.md --bibliography=bib.bib --reference-doc=reference.docx
+pandoc -o Dissert.docx Dissert.md --bibliography=bib.bib --reference-doc=reference.docx
 
-pandoc -o index.html Dissert.md --bibliography=bib.bib --mathjax --toc
+pandoc -o index.html Dissert.md --bibliography=bib.bib --mathjax --toc --standalone
+
+REM pandoc -o Dissert.pdf Dissert.md --bibliography=bib.bib --pdf-engine=xelatex
