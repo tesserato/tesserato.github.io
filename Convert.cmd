2a865de2d52fcc1bb8e34feb351784ad1ae26290
tesserato <tesserato@hotmail.com>
2018-07-02 07:48:26 -0300

    adicionado css

--- Convert.cmd
+++ Convert.cmd
@@ -1,5 +1,5 @@
 pandoc -o Dissert.docx Dissert.md --bibliography=bib.bib --reference-doc=reference.docx
 
-pandoc -o index.html Dissert.md --bibliography=bib.bib --mathjax --toc --standalone
+pandoc -o index.html Dissert.md --bibliography=bib.bib --mathjax --toc --standalone --css=style.css
 
 REM pandoc -o Dissert.pdf Dissert.md --bibliography=bib.bib --pdf-engine=xelatex
