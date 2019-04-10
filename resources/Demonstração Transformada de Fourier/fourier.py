2dccc2d7f6529950021b71352bccda598ce7b44a
tesserato <tesserato@hotmail.com>
2019-04-10 14:03:09 -0300

    cleaned code in fourier, separate plotting logic from calcaulations and other improvements

--- resources/Demonstração Transformada de Fourier/fourier.py
+++ resources/Demonstração Transformada de Fourier/fourier.py
@@ -1,65 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft
+# from scipy.fftpack import fft
 
 # Discrete Fourier Transform | Transformada Discreta de Fourier
-def DFT(x): 
-  N = x.shape[0]
-  # cria um vetor de N entradas complexas, preenchido com zeros
-  X = np.zeros(N, dtype='complex64')
-  n = np.array([p for p in range(N)]) # n = {0,1,2,...,N-1}
-  # Versão de "alta resolução" de n, usada para o gráfico apenas
-  Hn = np.linspace(0, N-1, 100*N) # Hn ={0,...,1,...,2,...,N-1}
-  for m in range(N): # m E {0,1,2,...,N-1}
-    C_Si = np.exp(-2 * np.pi * m * n / N * 1j) # C(m) - S(m) i
-    # Versão de "alta resolução" de C_Si, usada para o gráfico apenas
-    HC_Si = np.exp(-2 * np.pi * m * Hn / N * 1j)
-    X[m] = sum(np.multiply(x, C_Si)) # multiplicação termo a termo
-    # plotando:
-    plt.figure(1)
-    plt.suptitle('M =' + str(m), fontsize=16)
+def DFT(x, plot = False):
+  n = x.shape[0]  
+  FT = np.zeros(n, dtype='complex64') # cria um vetor de N entradas complexas, preenchido com zeros
+  N = np.array([p for p in range(n)]) # n = {0,1,2,...,N-1}
+  
+  if plot:# Versão de "alta resolução" de N, usada para o gráfico apenas
+    HN = np.linspace(0, n-1, 100*n) # Hn ={0,...,1,...,2,...,N-1}
+  for f in range(n): # m E {0,1,2,...,N-1}
 
-    plt.subplot(211)
-    # plt.plot(n,x[n].real, 'k:.', label='x[n]')
-    plt.plot(Hn,HC_Si.real, 'r-',label='C[n] ideal')
-    plt.plot(n,C_Si.real, 'k-o', label='C[n]')
-    # plt.ylabel('Real', fontsize=14, color='k')
-    cur_axes = plt.gca()
-    cur_axes.axes.get_xaxis().set_ticklabels([])
-    cur_axes.axes.get_yaxis().set_ticklabels([])
+    C_Si = np.exp(-2 * np.pi * f * N / n * 1j) # Cos(f) - Sin(f) i
+    FT[f] = sum(np.multiply(x, C_Si)) # multiplicação termo a termo
+    
+    if plot: # plotando:
+      HC_Si = np.exp(-2 * np.pi * f * HN / n * 1j) # Versão de "alta resolução" de C_Si, usada para o gráfico apenas
+      plt.figure(1)
+      plt.suptitle(f'f={f}', fontsize=16)
 
-    plt.subplot(212)
-    # plt.plot(n,x[n].imag, 'k:.', label='x[n]')
-    plt.plot(Hn,HC_Si.imag, 'r-',label='C[n] ideal')
-    plt.plot(n,C_Si.imag, 'k-o', label='C[n]')
-    # plt.ylabel('Imaginário', fontsize=14, color='k')
-    # plt.axis('off')
-    cur_axes = plt.gca()
-    # cur_axes.axes.get_xaxis().set_ticklabels([])
-    cur_axes.axes.get_yaxis().set_ticklabels([])
-    plt.legend()
-    plt.savefig('0' + str(m) + '.png', dpi=150, transparent="True", pad_inches=0)
-    plt.close('all')
-  #calcula o erro em relação ao algoritmo da biblioteca Scipy
-  error = sum((X-fft(x))**2)
+      plt.subplot(211)
+      # plt.plot(n,x[n].real, 'k:.', label='x[n]')
+      plt.plot(HN,HC_Si.real, 'r-',label=f'$\cos (2 \pi t {f} / {n})$ ideal')
+      plt.plot(N,C_Si.real, 'k--o', label=f'$\cos (2 \pi t {f} / {n})$ amostrado')
+      plt.xlabel('$t$', fontsize=14, color='k')
+      cur_axes = plt.gca()
+      cur_axes.axes.get_xaxis().set_ticklabels([])
+      cur_axes.axes.get_yaxis().set_ticklabels([])
+      plt.legend()
+
+      plt.subplot(212)
+      # plt.plot(n,x[n].imag, 'k:.', label='x[n]')
+      plt.plot(HN,HC_Si.imag, 'r-',label=f'$\sin (2 \pi t {f} / {n})$ ideal')
+      plt.plot(N,C_Si.imag, 'k--o', label=f'$\sin (2 \pi t {f} / {n})$ amostrado')
+      # plt.ylabel('Imaginário', fontsize=14, color='k')
+      # plt.axis('off')
+      cur_axes = plt.gca()
+      cur_axes.axes.get_xaxis().set_ticklabels([])
+      cur_axes.axes.get_yaxis().set_ticklabels([])
+      plt.legend()
+
+      plt.savefig('0' + str(f) + '.png', dpi=150, transparent="True", pad_inches=0)
+      plt.close('all')
+
+  FT = FT[0 : int(n/2+1)] if (n % 2 == 0) else FT[0 : int((n+1)/2)] # retorna a parte não redundante da transformada
+  error = np.sum(np.abs(FT - np.fft.rfft(x))) #calcula o erro em relação ao algoritmo da biblioteca numpy
   print(round(error,5))
-  # retorna a parte não redundante da transformada
-  return X[0 : int(N/2+1)] if (N % 2 == 0) else X[0 : int((N+1)/2)]
 
-def s(t):
-  return np.cos(2 * np.pi * t)
+  if plot:
+    plt.figure(1)
+    plt.subplot(211)
+    plt.ylabel('Real', fontsize=14, color='k')
+    plt.stem(FT.real,linefmt='k--',markerfmt='ko', basefmt='k--')
+    plt.subplot(212)
+    plt.stem(FT.imag,linefmt='k--',markerfmt='ks', basefmt='k--')
+    plt.ylabel('Imaginário', fontsize=14, color='k')
+    plt.savefig('transform.png', dpi=150)
+    plt.close('all')
+
+  return FT
+
+##########
 
 t = np.linspace(0, 1, 8) # 8 pontos entre 0 e 1
-x = s(t) # aplica a função ponto a ponto
-X = DFT(x)
-
-# plotando:
-plt.figure(1)
-plt.subplot(211)
-plt.ylabel('Real', fontsize=14, color='k')
-plt.stem(X.real,linefmt='k--',markerfmt='ko', basefmt='k--')
-plt.subplot(212)
-plt.stem(X.imag,linefmt='k--',markerfmt='ks', basefmt='k--')
-plt.ylabel('Imaginário', fontsize=14, color='k')
-plt.savefig('transform.png', dpi=150)
-plt.close('all')
+x = np.cos(2 * np.pi * t) # aplica a função ponto a ponto
+X = DFT(x, True)
